f2694670975fa47bc1ec1e804bb7c9063a5769dd
Patrick Barrett <pat.barrett926@gmail.com>
2021-06-22 23:07:50 -0500

    sql homework

--- EmployeeSQL/schema.sql
+++ EmployeeSQL/schema.sql
@@ -1,27 +1,12 @@
+DROP TABLE IF EXISTS employees;
 DROP TABLE IF EXISTS departments;
 DROP TABLE IF EXISTS dept_emp;
 DROP TABLE IF EXISTS dept_manager;
-DROP TABLE IF EXISTS employees;
 DROP TABLE IF EXISTS salaries;
 DROP TABLE IF EXISTS titles;
 
-CREATE TABLE departments (
-  dept_no varchar primary key NOT NULL,
-  dept_name varchar NOT NULL
-);
-
-CREATE TABLE dept_emp (
-  emp_no integer NOT NULL,
-  dept_no varchar NOT NULL
-);
-
-CREATE TABLE dept_manager (
-  dept_no varchar NOT NULL,
-  emp_no integer NOT NULL
-);
-
 CREATE TABLE employees (
-    emp_no integer NOT NULL,
+    emp_no integer NOT NULL primary key,
     emp_title_id varchar NOT NULL,
     birth_date date NOT NULL,
     first_name varchar NOT NULL,
@@ -30,9 +15,29 @@ CREATE TABLE employees (
     hire_date date NOT NULL
 );
 
+CREATE TABLE departments (
+  dept_no varchar NOT NULL primary key,
+  dept_name varchar NOT NULL
+);
+
+CREATE TABLE dept_emp (
+  emp_no integer NOT NULL,
+  dept_no varchar NOT NULL,
+  foreign key (emp_no) references employees(emp_no),
+  foreign key (dept_no) references departments(dept_no)
+);
+
+CREATE TABLE dept_manager (
+  dept_no varchar NOT NULL,
+  emp_no integer NOT NULL,
+  foreign key (dept_no) references departments(dept_no),
+  foreign key (emp_no) references employees(emp_no)
+);
+
 CREATE TABLE salaries (
   emp_no integer NOT NULL,
-  salary integer NOT NULL
+  salary integer NOT NULL,
+  foreign key (emp_no) references employees(emp_no)
 );
 
 CREATE TABLE titles (
